f6522e674f407722734615a8d8f09bc30c84b11a
KASSAS20 <kassas20@mail.ru>
2023-11-08 20:31:59 +0400

    workaet

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const container = document.querySelector('.container');
-
 const list_button =  [
-    "Esc", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0","-", "+", "Back",
+    "Esc", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0","-", "=", "Back",
     "Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\",
     "Caps", "A", "S", "D", "F", "G", "H", "J", "K", "L", ":", '"', "Enter",
     "Shift", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "Shift"
 ];
+
+const list_char = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0","-", "=",
+    "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\",
+    "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/",
+    "A", "S", "D", "F", "G", "H", "J", "K", "L"]
+
 const smugl = ["2", "W", "S", "X","9","O","L","."];
 const orangered= ["3", "E", "D", "C","8","I","K",","];
 const orange= ["4", "R", "F", "V","7","U","J","M"];
 const low_green= ["5", "T", "G", "B","6","Y","H","N"];
+
 
 function create_button(list_button){
     for (const key in list_button) {
@@ -39,12 +45,40 @@ function create_button(list_button){
             button.setAttribute('id', 'pink');
 
         }
-    
-        
-
+        button.setAttribute('name', list_button[key]);
         container.appendChild(button);
-
     }
 }
 
-create_button(list_button)
+
+function randChar(){
+    let randomIndex = Math.floor(Math.random() * list_char.length);
+    let randomValue = list_char[randomIndex];
+    return randomValue;
+}
+
+
+function dinamic(name = randChar()){
+    const but = document.getElementsByName(name)[0];
+    but.classList.toggle('scale-up');
+
+
+    function clicked(){
+            but.classList.remove('scale-up')
+            but.removeEventListener('keydown', key_click)
+            but.removeEventListener('click', clicked)
+            dinamic(randChar())
+        }
+        
+    function key_click(event){
+        if (event.key .toUpperCase() == name){
+            but.click()
+        }
+     }
+    document.addEventListener('keydown', key_click)
+    but.addEventListener("click", clicked)
+    }
+
+
+create_button(list_button);
+dinamic();
